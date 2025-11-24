d44e76bf76aa629becdcb234ad4f60131b4effe2
haydenfernandes <131270907+haydenfernandes@users.noreply.github.com>
2025-11-24 11:31:29 +0000

    this will the be the setup for the screen

--- game.py
+++ game.py
@@ -1,1 +1,17 @@
-import pygame
+import pygame,sys
+
+pygame.init()
+screenwidth, screenheight = 600, 600 # set the screen size
+screen = pygame.display.set_mode((screenwidth, screenheight))
+pygame.display.set_caption("space invaders")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((0, 0, 0)) # fill the screen with black
+    pygame.display.flip()  # update the display
+    clock.tick(60)
