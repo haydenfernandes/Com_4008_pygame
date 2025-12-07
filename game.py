066839fdb1505a50b232cafb614333b531b3bd38
hasnainjavedyt-cpu <hasnainjavedyt@gmail.com>
2025-12-07 18:35:35 +0000

    Implement obstacle creation in game
    Added obstacle creation functionality with multiple obstacles.

--- game.py
+++ game.py
@@ -13,14 +13,43 @@ class Game:
         player_sprite = Player((300,500)) 
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
+    #obstacle setup
+    self.shape = obstacle.shape
+    self.block_size = 6
+    self.blocks = pygame.sprite.group
+    self.obstacle_amout = 4
+    self.obstacle_x_position = [num * (screen width / self.obstacle_amount)for num in range(self.obstacle_amount)]
+    self.creat_multiple_obstacles(* self.obstacle_x_positions, x_start = screen_width / 15, y=start = 480, )
+
+def create_obstacle(self, x_start y_start,offset_x):
+    for row_index, row in enumerate(self.shape):
+        for col_index.col in enumerate(row):
+            if col == 'x':
+                x = x_start + col_index * self.block_size + offset_x
+                y = y_start + row_index * self.block_size 
+                block = obstacle.block(self.block_size,(255,89,70),x,y)
+                self.blocks.add(block)
+
+    def create_multiple_obstacles(self,*offset,x_start,y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start,offset_x)
+            
+                
+    
+    
+    
+
     def run(self): # main game loop
 
         
         self.player.update()    
 
         self.player.sprite.lasers.draw(screen)  # draw the player's lasers on the screen 
-        self.player.draw(screen)  # draw the player sprite on the screen
-        
+        self.player.draw(screen) # draw the player sprite on the screen
+        self.blocks.draw(screen)
+
+
+
 
 if __name__ == "__main__":
     pygame.init()
